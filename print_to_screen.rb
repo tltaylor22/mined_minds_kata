4d41e8d62fd47dcaca0156fccacc20943282bd11
Tim Taylor <ttaylor@clarion.edu>
2017-01-16 15:01:29 -0500

    mod 3 & 5

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,7 +1,11 @@
 number = 1
 
 100.times do
-	if number % 3 == 0
+	if number % 3 == 0 and number % 5 ==0
+	# or you could use:
+	#if number % 15 == 0
+		puts "mined minds"
+	elsif number % 3 == 0
 		puts "mined"
 	elsif number % 5 == 0
 		puts "mind"
