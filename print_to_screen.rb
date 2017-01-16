94b554d8154b74946ed0e285859b808bd7137880
Tim Taylor <ttaylor@clarion.edu>
2017-01-16 14:08:20 -0500

    3 & 5 = mined minds

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,12 +1,12 @@
 number = 1
-until number > 100
-  puts number
-  number += 1
-end
 
-number = 1
-100.times do |variable|
-	puts number
+100.times do
+	if number == 3
+		puts "mined"
+	elsif number == 5
+		puts "mind"
+	else
+		puts number
+	end
 	number = number + 1
 end
-
