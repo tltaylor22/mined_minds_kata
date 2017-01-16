a6cb81c13bdf75294f53a283e610ab5da3a284c6
Tim Taylor <ttaylor@clarion.edu>
2017-01-16 16:15:22 -0500

    test one returns as one

--- test_mined_minds_functions.rb
+++ test_mined_minds_functions.rb
@@ -1,8 +1,12 @@
 require 'minitest/autorun'
 
+def get_mined_minds_result(number)
+	1
+end
+
 class TestMMFunctions < Minitest::Test 
 	def test_one_equals_one()
-		assert_equal(1, 1)
+		assert_equal(1, get_mined_minds_result(1))
 	end
 
 end
