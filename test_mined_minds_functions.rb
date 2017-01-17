feb4cc24a2fabb7ba8573b3cf66bfb3ad22f756e
Tim Taylor <ttaylor@clarion.edu>
2017-01-16 19:31:47 -0500

    test 6 returns results for 6

--- test_mined_minds_functions.rb
+++ test_mined_minds_functions.rb
@@ -20,4 +20,9 @@ class TestMMFunctions < Minitest::Test
 	def test_five_returns_mined_minds()
 		assert_equal('mined minds', get_mined_minds_result(15))
 	end
+
+	def test_six_returns_mined()
+		assert_equal('mined', get_mined_minds_result(6))
+	end
+
 end
